a7a68ec2be76db21796ddca496a3448291fb5e8a
DNS <glotov-aleks@mail.ru>
2023-04-29 19:51:34 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i=1;i<10;i++){
+       task1();
+       task2();
+    }
+    public static void task1() {
+        System.out.println("задача 1 ");
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task2(){
+        System.out.println("\nзадача 1 ");
+        for (int i =10; i > 0; i--) {
             System.out.println(i);
         }
     }
