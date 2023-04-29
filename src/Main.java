dd26be3b6ceb2264c307f0cf71302269f18991e6
DNS <glotov-aleks@mail.ru>
2023-04-29 20:05:45 +0500

    task6

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
 
     }
 
@@ -40,6 +41,13 @@ public class Main {
         System.out.println("\nзадача 5 ");
         for (int i = 1904; i < 2096; i += 4) {
             System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("\nзадача 6 ");
+        for (int i = 7; i < 100; i += 7) {
+            System.out.println(i);
         }
     }
 }
